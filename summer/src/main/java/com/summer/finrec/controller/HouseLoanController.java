c9fb332b2c6dd6649790e6eaee6218338f28fb7f
hyejin <decaffeine12@gmail.com>
2018-06-14 18:24:16 +0900

    HouseLoanController Minor Update

--- summer/src/main/java/com/summer/finrec/controller/HouseLoanController.java
+++ summer/src/main/java/com/summer/finrec/controller/HouseLoanController.java
@@ -2,17 +2,19 @@ package com.summer.finrec.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
 public class HouseLoanController {
 	Logger log = LoggerFactory.getLogger(this.getClass());
 	
-	//finrec/houseLoan/doSelectList.do
+	//finrec/houseloan/doSelectList.do
 	@RequestMapping(value="/finrec/houseLoan/doSelectList.do",
 			method=RequestMethod.GET)
 	public String getSelectList( ) {return "finrec/houseLoanSearchList";}
 	
-	//finrec/houseLoan/doSelectOne.do
+	//finrec/houseloan/doSelectOne.do
 	public String getSelectOne() {return "";}
 }
