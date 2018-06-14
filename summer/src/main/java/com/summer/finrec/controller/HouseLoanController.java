06a5938566c98aafffb661ca966972867a9a1fd8
hyejin <decaffeine12@gmail.com>
2018-06-14 18:18:22 +0900

    FinRec pages Initial Upload & FinRec Controllers Update

--- summer/src/main/java/com/summer/finrec/controller/HouseLoanController.java
+++ summer/src/main/java/com/summer/finrec/controller/HouseLoanController.java
@@ -2,12 +2,16 @@ package com.summer.finrec.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 public class HouseLoanController {
 	Logger log = LoggerFactory.getLogger(this.getClass());
 	
 	//finrec/houseLoan/doSelectList.do
-	public String getSelectList( ) {return "";}
+	@RequestMapping(value="/finrec/houseLoan/doSelectList.do",
+			method=RequestMethod.GET)
+	public String getSelectList( ) {return "finrec/houseLoanSearchList";}
 	
 	//finrec/houseLoan/doSelectOne.do
 	public String getSelectOne() {return "";}
