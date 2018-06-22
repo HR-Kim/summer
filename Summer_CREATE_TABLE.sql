07a907d81c7cbe5c209ea43b275f4814b65bbd25
Hyejin Kim <decaffeine12@gmail.com>
2018-06-22 16:32:15 +0900

    Update Summer_CREATE_TABLE.sql

--- Summer_CREATE_TABLE.sql
+++ Summer_CREATE_TABLE.sql
@@ -198,4 +198,116 @@ ALTER TABLE "SU_CODES"
 -- 사용여부
 COMMENT ON COLUMN "SU_CODES"."CD_USE_YN" IS '0 미사용 1 사용';
 
+-- 금융상품즐겨찾기
+ALTER TABLE "SU_FINFAVS"
+	DROP CONSTRAINT "PK_SU_FINFAVS"; -- 금융상품즐겨찾기 기본키
+
+-- 금융상품즐겨찾기
+DROP TABLE "SU_FINFAVS";
+
+-- 금융상품즐겨찾기
+CREATE TABLE "SU_FINFAVS" (
+	"FAVS_NO"     NUMBER(5)         NOT NULL, -- 즐겨찾기번호
+	"ID"          VARCHAR2(20 BYTE) NOT NULL, -- 회원ID
+	"FIN_PRDT_CD" VARCHAR2(30 BYTE) NOT NULL  -- 금융상품코드
+);
+
+-- 금융상품즐겨찾기 기본키
+CREATE UNIQUE INDEX "PK_SU_FINFAVS"
+	ON "SU_FINFAVS" ( -- 금융상품즐겨찾기
+		"FAVS_NO" ASC -- 즐겨찾기번호
+	);
+
+-- 금융상품즐겨찾기
+ALTER TABLE "SU_FINFAVS"
+	ADD
+		CONSTRAINT "PK_SU_FINFAVS" -- 금융상품즐겨찾기 기본키
+		PRIMARY KEY (
+			"FAVS_NO" -- 즐겨찾기번호
+		);
+
+-- 정기예금
+ALTER TABLE "SU_DEPOSIT"
+	DROP CONSTRAINT "PK_SU_DEPOSIT"; -- 정기예금 기본키
+
+-- 정기예금
+DROP TABLE "SU_DEPOSIT";
+
+-- 정기예금
+CREATE TABLE "SU_DEPOSIT" (
+	"FIN_PRDT_CD"       VARCHAR2(50 CHAR)  NOT NULL, -- 금융상품코드
+	"FIN_CO_NO"         VARCHAR2(50 CHAR)  NOT NULL, -- 금융회사코드
+	"KOR_CO_NM"         VARCHAR2(50 CHAR)  NOT NULL, -- 금융회사명
+	"FIN_PRDT_NM"       VARCHAR2(50 CHAR)  NOT NULL, -- 금융상품명
+	"JOIN_WAY"          VARCHAR2(50 CHAR)  NOT NULL, -- 가입방법
+	"MTRT_INT"          VARCHAR2(200 CHAR) NULL, -- 만기후이자율
+	"SPCL_CND"          VARCHAR2(200 CHAR) NOT NULL, -- 우대조건
+	"ETC_NOTE"          CLOB               NULL,     -- 기타유의사항
+	"INTR_RATE_MIN"     NUMBER(15,5)       NOT NULL, -- 저축금리최소
+	"INTR_RATE_MAX"     NUMBER(15,5)       NOT NULL, -- 저축금리최대
+	"INTR_RATE_TYPE"    VARCHAR2(5 CHAR)   NOT NULL, -- 저축금리유형
+	"INTR_RATE_TYPE_NM" VARCHAR2(30 CHAR)  NOT NULL  -- 저축금리유형명
+)
+INITRANS 3;
+
+-- 정기예금 기본키
+CREATE UNIQUE INDEX "PK_SU_DEPOSIT"
+	ON "SU_DEPOSIT" ( -- 정기예금
+		"FIN_PRDT_CD" ASC -- 금융상품코드
+	);
+
+-- 정기예금
+ALTER TABLE "SU_DEPOSIT"
+	ADD
+		CONSTRAINT "PK_SU_DEPOSIT" -- 정기예금 기본키
+		PRIMARY KEY (
+			"FIN_PRDT_CD" -- 금융상품코드
+		);
+
+
+
+
+
+
+-- 적금
+ALTER TABLE "SU_SAVING"
+	DROP CONSTRAINT "PK_SU_SAVING"; -- 적금 기본키
+
+-- 적금
+DROP TABLE "SU_SAVING";
+
+-- 적금
+CREATE TABLE "SU_SAVING" (
+	"FIN_PRDT_CD"       VARCHAR2(50 CHAR)  NOT NULL, -- 금융상품코드
+	"FIN_CO_NO"         VARCHAR2(50 CHAR)  NOT NULL, -- 금융회사코드
+	"KOR_CO_NM"         VARCHAR2(50 CHAR)  NOT NULL, -- 금융회사명
+	"FIN_PRDT_NM"       VARCHAR2(50 CHAR)  NOT NULL, -- 금융상품명
+	"JOIN_WAY"          VARCHAR2(50 CHAR)  NOT NULL, -- 가입방법
+	"MTRT_INT"          VARCHAR2(200 CHAR) NULL, -- 만기후이자율
+	"SPCL_CND"          VARCHAR2(200 CHAR) NOT NULL, -- 우대조건
+	"ETC_NOTE"          CLOB               NULL,     -- 기타유의사항
+	"INTR_RATE_MIN"     NUMBER(15,5)       NOT NULL, -- 저축금리최소
+	"INTR_RATE_MAX"     NUMBER(15,5)       NOT NULL, -- 저축금리최대
+	"INTR_RATE_TYPE"    VARCHAR2(5 CHAR)   NOT NULL, -- 저축금리유형
+	"RSRV_TYPE"         VARCHAR2(5 CHAR)   NOT NULL, -- 적립유형
+	"RSRV_TYPE_NM"      VARCHAR2(30 CHAR)  NOT NULL, -- 적립유형명
+	"INTR_RATE_TYPE_NM" VARCHAR2(30 CHAR)  NOT NULL  -- 저축금리유형명
+)
+INITRANS 3;
+
+-- 적금 기본키
+CREATE UNIQUE INDEX "PK_SU_SAVING"
+	ON "SU_SAVING" ( -- 적금
+		"FIN_PRDT_CD" ASC -- 금융상품코드
+	);
+
+-- 적금
+ALTER TABLE "SU_SAVING"
+	ADD
+		CONSTRAINT "PK_SU_SAVING" -- 적금 기본키
+		PRIMARY KEY (
+			"FIN_PRDT_CD" -- 금융상품코드
+		);
+		
+		
 
